604ace9e76e040eb5fa04809b9e7ac579949c9ed
Bukunmikuti <bukunmikuti@gmail.com>
2022-05-31 19:12:31 +0100

    Initial commit (tested and stable)

---
+++ bibleup.php
@@ -0,0 +1,136 @@
+<?php
+/*
+   Plugin Name: BibleUp
+   Plugin URI: https://bibleup.netlify.com
+   description: BibleUp transforms Bible References on a webpage into flexible, and highly customisable popovers.  
+   Version: 1.0
+   Author: BibleUp
+   Author URI: https://bibleup.netlify.com
+   License: GPLv3
+   License URI: https://www.gnu.org/licenses/gpl-3.0.html
+   */
+
+class BibleUp {
+	
+	private $plugin_path; // @var string
+	private $wpsf; // @var WordPressSettingsFramework
+	private $script; // CDN delivered and source-controlled
+
+	// BibleUp constructor.
+	function __construct() {
+		$this->plugin_path = plugin_dir_path( __FILE__ );
+		$this->script = 'https://cdn.jsdelivr.net/npm/@bibleup/bibleup@beta'; //pegged to major version
+
+		// Include and create a new WordPressSettingsFramework
+		require_once( $this->plugin_path . 'wp-settings-framework/wp-settings-framework.php' );
+		$this->wpsf = new WordPressSettingsFramework( $this->plugin_path . 'settings/settings-general.php', 'prefix_settings_general' );
+
+		// Add admin menu
+		add_action( 'admin_menu', array( $this, 'add_settings_page' ), 20 );
+		
+		// Add an optional settings validation filter (recommended)
+		add_filter( $this->wpsf->get_option_group() . '_settings_validate', array( &$this, 'validate_settings' ) );
+	}
+
+	/**
+	 * Add settings page.
+	 */
+	function add_settings_page() {
+		$this->wpsf->add_settings_page( array(
+			'parent_slug' => 'options-general.php',
+			'page_title'  => __( 'BibleUp', 'bibleup' ),
+			'menu_title'  => __( 'BibleUp', 'bibleup' ),
+			'capability'  => 'manage_options',
+		));
+	}
+
+	/**
+	 * Validate settings.
+	 * @param $input
+	 * @return mixed
+	 */
+	function validate_settings( $input ) {
+		// Do your settings validation here
+		// Same as $sanitize_callback from http://codex.wordpress.org/Function_Reference/register_setting
+		return $input;
+	}
+	
+	function start() {
+		$raw_options = wpsf_get_setting( 'prefix_settings_general', 'tab_2_paste_config', 'raw_options' );
+		if (empty($raw_options)) {
+			add_action('wp_footer', array( $this, 'get_select_options' ));
+		} else {
+			add_action('wp_footer', array( $this, 'get_raw_options' ));
+		}
+	}
+
+	function get_select_options() {
+		// tab_1 section ID - select_options
+		$popup = wpsf_get_setting( 'prefix_settings_general', 'tab_1_select_options', 'popup' );
+		$version = wpsf_get_setting( 'prefix_settings_general', 'tab_1_select_options', 'version' );
+		$dark_theme = wpsf_get_setting( 'prefix_settings_general', 'tab_1_select_options', 'dark_theme' );
+		// tab_1 section ID - popup_style
+		$primary = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'primary' );
+		$secondary = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'secondary' );
+		$tertiary = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'tertiary' );
+		$header_color = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'header_color' );
+		$font_color = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'font_color' );
+		$version_color = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'version_color' );
+		$close_color = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'close_color' );
+		$border_radius = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'border_radius' );
+		$box_shadow = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'box_shadow' );
+		$font_size = wpsf_get_setting( 'prefix_settings_general', 'tab_1_popup_style', 'font_size' );
+		// tab_1 section ID - additional
+		$bu_allow = wpsf_get_setting( 'prefix_settings_general', 'tab_1_additional', 'bu_allow' );
+		$bu_ignore = wpsf_get_setting( 'prefix_settings_general', 'tab_1_additional', 'bu_ignore' );
+		// tab_2 section ID - paste_config
+		
+		$call = function($prop, $default) {
+			if ($prop == 'false') {
+				return $default;
+			} else if ($prop == 'true') {
+				return 'true';
+			} else {
+				return "'${prop}'";
+			}
+		};
+		
+		echo "
+		<script src='$this->script'></script>
+    	<script>
+			let b = new BibleUp(document.body, {
+  				popup: ". $call($popup, null) .",
+				version: ". $call($version, null) .",
+				darkTheme: ". $call($dark_theme, null) .",
+				bu_ignore: ". $call($bu_ignore, '["H1", "H2", "H3", "H4", "H5", "H6", "IMG", "A"]') .",
+				bu_allow: ". $call($bu_allow, '[]') .",
+				styles: {
+					primary: ". $call($primary, 'false') .",
+					secondary: ". $call($secondary, 'false') .", 
+					tertiary: ". $call($tertiary, 'false') .",
+					headerColor: ". $call($header_color, 'false') .",
+					color: [". $call($font_color, 'false') .", ". $call($version_color, 'false') .", ". $call($close_color, 'false') ."],
+					borderRadius: ". $call($border_radius, 'false') .",
+					boxShadow: ". $call($box_shadow, 'false') .",
+					fontSize: ". $call($font_size, 'false') .",
+				}
+			})
+			b.create();
+		</script>";
+	}
+	
+	function get_raw_options() {
+		$raw_options = wpsf_get_setting( 'prefix_settings_general', 'tab_2_paste_config', 'raw_options' );
+		echo "
+		<script src='$this->script'></script>
+		<script>
+			let b = new BibleUp(document.body, ".$raw_options.");
+			b.create();
+		</script>
+		";
+	}
+	
+}
+
+$bibleup = new BibleUp();
+$bibleup->start();
---
+++ settings/settings-general.php
@@ -0,0 +1,259 @@
+<?php
+/**
+ * WordPress Settings Framework
+ *
+ * @author  Gilbert Pellegrom, James Kemp
+ * @link    https://github.com/gilbitron/WordPress-Settings-Framework
+ * @license MIT
+ */
+
+/**
+ * Define your settings
+ *
+ * The first parameter of this filter should be wpsf_register_settings_[options_group],
+ * in this case "my_example_settings".
+ *
+ * Your "options_group" is the second param you use when running new WordPressSettingsFramework()
+ * from your init function. It's important as it differentiates your options from others.
+ *
+ * To use the tabbed example, simply change the second param in the filter below to 'wpsf_tabbed_settings'
+ * and check out the tabbed settings function on line 156.
+ */
+
+add_filter( 'wpsf_register_settings_prefix_settings_general', 'wpsf_tabbed_settings' );
+
+/**
+ * Tabbed example.
+ * @param array $wpsf_settings settings.
+ */
+/**
+ * Tabbed example.
+ *
+ * @param array $wpsf_settings settings.
+ */
+function wpsf_tabbed_settings( $wpsf_settings ) {
+	// Tabs.
+	$wpsf_settings['tabs'] = array(
+		array(
+			'id'    => 'tab_01',
+				'title' => esc_html__( 'Note', 'bibleup' ),
+		),
+		array(
+			'id'    => 'tab_1',
+			'title' => esc_html__( 'Select Options', 'bibleup' ),
+		),
+		array(
+			'id'    => 'tab_2',
+			'title' => esc_html__( 'Paste Config', 'bibleup' ),
+		),
+	);
+
+	// Settings.
+	$wpsf_settings['sections'] = array(
+		array(
+			'tab_id'        => 'tab_1',
+			'section_id'    => 'note',
+			'section_description' => '<b>Please read the following first before you begin to configure BibleUp</b><br>
+			There are two methods you can use to configure BibleUp - <b>select Options</b> or <b>Paste Config</b><br>
+			Use the <b>Select Options</b> forms below to set your options manually or the <b>Paste Config</b> to paste generated config options from the editor (Recommended)<br><br>
+			Set any option to <b>false</b> to get the default value<br>
+			For full documentation of these options, check the <a href="https://bibleup.netlify.app/docs">docs</a>',
+			'section_title' => 'NOTE',
+			'section_order' => 10,
+		),
+		array(
+			'tab_id'        => 'tab_1',
+			'section_id'    => 'select_options',
+			'section_title' => 'Select Options',
+			'section_order' => 10,
+			'fields'        => array(
+				array(
+					'id'      => 'popup',
+					'title'   => 'Popup Type',
+					'desc'    => 'The preferred popup type.',
+					'type'    => 'select',
+					'default' => 'classic',
+					'choices' => array(
+						'classic'   => 'classic',
+						'inline' => 'inline',
+						'wiki'  => 'wiki',
+					),
+				),
+				array(
+					'id'      => 'version',
+					'title'   => 'Version',
+					'desc'    => 'The default Bible translation for references.',
+					'type'    => 'select',
+					'default' => 'KJV',
+					'choices' => array(
+						'KJV'   => 'King James Version (KJV)',
+						'ASV' => 'American Standard Version (ASV)',
+						'LSV'  => 'Literal Standard Version (LSV)',
+						'WEB'  => 'World English Bible (WEB)',
+					),
+				),
+				array(
+					'id'      => 'dark_theme',
+					'title'   => 'Dark Theme',
+					'desc'    => 'Enable dark theme on popup.<br>
+					This can be overriden by the background and font color set by other options, to preserve the default dark theme you must set other color options to false.',
+					'type'    => 'radio',
+					'default' => 'false',
+					'choices' => array(
+						'true'   => 'True',
+						'false' => 'False',
+					),
+				),
+			),
+		),
+        
+		array(
+			'tab_id'        => 'tab_1',
+			'section_id'    => 'popup_style',
+			'section_title' => 'Popup Style',
+			'section_description' => 'Set popup background and font colors.',
+			'section_order' => 10,
+			'fields'        => array(
+				array(
+					'id'      => 'primary',
+					'title'   => 'Primary',
+					'desc'    => 'Set color for overall popup background',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'secondary',
+					'title'   => 'Secondary',
+					'desc'    => 'Set background color for popup header (if it exists)',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'tertiary',
+					'title'   => 'Tertiary',
+					'desc'    => 'Set background color for popup version (if it exists)',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'header_color',
+					'title'   => 'Header Color',
+					'desc'    => 'Font color for popup header (if it exists).<br>
+					This will override the default font color for text in the header only',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'font_color',
+					'title'   => 'Font Color',
+					'desc'    => 'The default font color for the popup',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'version_color',
+					'title'   => 'Version Color',
+					'desc'    => 'Font color for popup version (if it exists).<br>
+					This will override the default font color for text in the version box only',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'close_color',
+					'title'   => 'Close Color',
+					'desc'    => 'Set color for close button (if it exists).<br>
+					The close button only exists on popup type "wiki" ',
+					'type'    => 'color',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'border_radius',
+					'title'   => 'Border Radius',
+					'desc'    => 'Set border radius for popup (in units)',
+					'type'    => 'text',
+					'placeholder' => '5px',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'box_shadow',
+					'title'   => 'Box Shadow',
+					'desc'    => 'Set a drop shadow for the popup (using CSS syntax)',
+					'type'    => 'text',
+					'placeholder' => '2px 6px 10px #404040',
+					'default' => 'false',
+				),
+				array(
+					'id'      => 'font_size',
+					'title'   => 'Font Size',
+					'desc'    => 'Set font size for all popup (text in units)',
+					'type'    => 'text',
+					'placeholder' => '16px',
+					'default' => 'false',
+				),
+			),
+		),
+
+        array(
+			'tab_id'        => 'tab_1',
+			'section_id'    => 'additional',
+			'section_title' => 'Addition Settings',
+			'section_description' => 'Use these options to configure BibleUp behaviour.',
+			'section_order' => 10,
+			'fields'        => array(
+				array(
+					'id'      => 'bu_ignore',
+					'title'   => 'Ignore Specific HTML Elements',
+					'desc'    => "BibleUp won't tag element listed here.<br>
+					Seperate these elements using a comma and put inside a square bracket.",
+					'type'    => 'text',
+					'placeholder'    => "['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'IMG', 'A']",
+					'default' => 'false',
+					'link'     => array(
+						'url'      => esc_url( 'https://bibleup.netlify.app/docs/guide/options.html#bu-ignore' ),
+						'type'     => 'link', // Can be 'tooltip' or 'link'. Default is 'tooltip'.
+						'text'     => 'Learn More',
+						'external' => true,
+					),
+				),
+				array(
+					'id'      => 'bu_allow',
+					'title'   => 'Allow Specific HTML Elements',
+					'desc'    => "Override the default elements ignored by BibleUp by placing them here.<br>
+					Seperate these elements using a comma and put inside a square bracket.",
+					'type'    => 'text',
+					'placeholder'    => "['H1', 'H2']",
+					'default' => '',
+					'link'     => array(
+						'url'      => esc_url( 'https://bibleup.netlify.app/docs/guide/options.html#bu-allow' ),
+						'type'     => 'link', // Can be 'tooltip' or 'link'. Default is 'tooltip'.
+						'text'     => 'Learn More',
+						'external' => true,
+					),
+				),
+			),
+        ),
+			
+		array(
+			'tab_id'        => 'tab_2',
+			'section_id'    => 'paste_config',
+			'section_title' => 'Paste Config',
+			'section_description' => 'Use the instant <a target="_blank" href="https://bibleup.netlify.app/demo#editor">editor</a> to style popup in real-time, then copy and paste the generated config options.<br>
+			This section <b>overides every options</b> in the <b>Select Options</b> tab.<br><br>
+			Leave the editor blank if you want to manually seclect options instead.',
+			'section_order' => 10,
+			'fields'        => array(
+				array(
+					'id'      => 'raw_options',
+					'title'   => 'Raw Options',
+					'desc'    => 'Paste the raw config options here',
+					'type'        => 'code_editor',
+					'mimetype'    => 'javascript',
+					'default' => 'false',
+				),
+			),
+		),
+	);
+
+	return $wpsf_settings;
+}
+
